fc35c5d18bea24bd46ce2f1df59cd9bb1f47918e
Joshua Barrass <josh.barrass.work@gmail.com>
2023-01-04 14:30:15 +0000

    Graft point output code

--- src/Output.hs
+++ src/Output.hs
@@ -1,14 +1,44 @@
 module Output
   ( filelistOutput
+  , graftPointOutput
   ) where
 
 import Data.List
+import System.FilePath
 import Disc
+
+getFilenameLists :: [Disc] -> [[String]]
+getFilenameLists discs = let
+  allFiles = map files discs
+  in map (map filename) allFiles
+
+concatToFilelists :: [[String]] -> [String]
+concatToFilelists = map ((++ "\n") . intercalate "\n")
 
 -- each string in the output list is one output file
 filelistOutput :: [Disc] -> [String]
 filelistOutput discs = let
-  allFiles = map files discs
-  filenameList = map (map filename) allFiles
-  in map ((++ "\n") . intercalate "\n") filenameList
+  filenameList = getFilenameLists discs
+  in concatToFilelists filenameList
+
+-- replaces all backslash and equals characters with escaped versions
+escapeGraftPoint :: String -> String
+escapeGraftPoint [] = []
+escapeGraftPoint ('\\':xs) = '\\' : '\\' : escapeGraftPoint xs
+escapeGraftPoint ('=':xs) = '\\' : '=' : escapeGraftPoint xs
+escapeGraftPoint (x:xs) = x : escapeGraftPoint xs
+
+-- generate graft point syntax for a single filename
+makeGraftPoint :: String -> String
+makeGraftPoint p = let
+  escaped = escapeGraftPoint p
+  dirname = takeDirectory escaped
+  graftPoint = dirname ++ "/"
+  in graftPoint ++ '=' : escaped
   
+
+graftPointOutput :: [Disc] -> [String]
+graftPointOutput discs = let
+  filenameList = getFilenameLists discs
+  graftPointList = map (map makeGraftPoint) filenameList
+  in concatToFilelists graftPointList
